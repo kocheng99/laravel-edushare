476e206ffe387a48aa2a737a4c6da9bcd897f298
kocheng99 <imamsulthonthon@gmail.com>
2020-09-06 23:00:21 +0700

    modifikasi penulisan resource file atau link dari css ataupun js, yang tadinya cdn menjadi file sendiri

--- resources/views/admin/admin_layout/main.blade.php
+++ resources/views/admin/admin_layout/main.blade.php
@@ -5,11 +5,13 @@
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
 {{-- <title>@yield('title') - {{config('app.name')}}</title> --}}
 <title>@yield('title')</title>
-
+ 
   <!-- General CSS Files -->
-<link rel="stylesheet" href="{{asset('https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css')}}" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+{{-- <link rel="stylesheet" href="{{asset('https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css')}}" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> --}}
+<link rel="stylesheet" href="{{asset('/assets/bootstrap/css/bootstrap.min.css')}}">
 <link rel="stylesheet" href="{{asset('https://use.fontawesome.com/releases/v5.7.2/css/all.css')}}" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-
+{{-- <link rel="stylesheet" href="{{asset('/assets/modules/fontawesome/css/all.min.css')}}"> --}}
+ 
 
   <!-- CSS Libraries -->
 
@@ -57,11 +59,16 @@
   @stack('before-script')
 
   <!-- General JS Scripts -->
-  <script src="{{asset('https://code.jquery.com/jquery-3.3.1.min.js')}}" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-  <script src="{{asset('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js')}}" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="{{asset('https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js')}}" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <script src="{{asset('https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js')}}"></script>
-  <script src="{{asset('https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js')}}"></script>
+  {{-- <script src="{{asset('https://code.jquery.com/jquery-3.3.1.min.js')}}" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script> --}}
+  <script src="{{asset('/assets/modules/jquery.min.js')}}"></script>
+  {{-- <script src="{{asset('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js')}}" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script> --}}
+  <script src="{{asset('/assets/modules/popper.js')}}"></script>
+  {{-- <script src="{{asset('https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js')}}" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> --}}
+  <script src="{{asset('/assets/bootstrap/js/bootstrap.min.js')}}"></script>
+  {{-- <script src="{{asset('https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js')}}"></script> --}}
+  <script src="{{asset('/assets/modules/jquery.nicescroll.min.js')}}"></script>
+  {{-- <script src="{{asset('https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js')}}"></script> --}}
+  <script src="{{asset('/assets/modules/moment.min.js')}}"></script>
   <script src="{{asset('/assets/js/stisla.js')}}"></script>
 
   <!-- JS Libraies -->
